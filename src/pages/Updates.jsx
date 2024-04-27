e5f641d2119bf0e37b78b7eb2bd247b573452c42
berkanozm <ozmberkan@gmail.com>
2024-04-27 14:48:53 +0300

    v1.0.2

--- src/pages/Updates.jsx
+++ src/pages/Updates.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { IoBackspace } from "react-icons/io5";
+import { IoMdHome } from "react-icons/io";
+
 import { Link } from 'react-router-dom';
 import HizliEmlakLogo from '../assets/HizliEmlakLogo.svg'
 export default function Updates() {
 
     return (
-        <div>
-            <div className='flex justify-start items-center gap-x-5'>
+        <div clas>
+            <div className='flex items-center justify-end gap-x-5'>
                 <img src={HizliEmlakLogo} alt="" />
+                <Link to="/" className='bg-[#EF0000]/10 w-10 h-10 rounded-full flex justify-center items-center '><IoMdHome size={20} className='text-zinc-800' /></Link>
+            </div>
+            <div className='flex justify-start items-center gap-x-5'>
                 <b>v1.0.1</b>
                 <i>updates - 24.04.2024 </i>
-                <Link to="/"><IoBackspace size={28} /></Link>
             </div>
             <hr />
             <ul className='flex flex-col my-4'>
@@ -19,7 +22,17 @@ export default function Updates() {
                 <li className='list-disc'>Added Sidebar</li>
                 <li className='list-disc'>Added Detail</li>
                 <li className='list-disc'>Added Filter (search)</li>
-
+            </ul>
+            <div className='flex justify-start items-center gap-x-5'>
+                <b>v1.0.2</b>
+                <i>updates - 27.04.2024 </i>
+            </div>
+            <hr />
+            <ul className='flex flex-col my-4'>
+                <li className='list-disc'>Added Images</li>
+                <li className='list-disc'>Added Slick</li>
+                <li className='list-disc'>Half DetailPage</li>
+                <li className='list-disc'>Added Updates Page / Button</li>
             </ul>
         </div>
     )
